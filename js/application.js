079f6fabca4a5b1bd085522f986bfdbc02899c64
Chuck Lauer Vose <vosechu@gmail.com>
2013-03-16 16:40:45 -0700

    finished views video

--- js/application.js
+++ js/application.js
@@ -1,6 +1,7 @@
 var NotesApp = (function () {
   var App = {
-    stores: {}
+    stores: {},
+    views: {}
   };
 
   // Initialize localStorage
@@ -19,11 +20,38 @@ var NotesApp = (function () {
     }
   });
 
+  var NewFormView = Backbone.View.extend({
+    events: {
+      "submit form": "createNote"
+    },
+
+    createNote: function (e) {
+      e.preventDefault();
+
+      var attrs = this.getAttributes,
+          note = new Note();
+
+      note.set(attrs);
+      note.save();
+    },
+
+    getAttributes: function () {
+      return {
+        title: this.$('form [name=title]').val(),
+        body: this.$('form [name=body]').val()
+      };
+    }
+  });
+
   var Notes = Backbone.Collection.extend({
 
   });
 
-  window.Note = Note;
+  $(document).ready(function () {
+    App.views.new_form = new NewFormView({
+      el: $('#new')
+    });
+  });
 
   return App;
 })();
