1847cd15abbcb45317eb842e996ac6a750411a63
Chuck Lauer Vose <vosechu@gmail.com>
2013-03-16 15:52:54 -0700

    adding note

---
+++ js/application.js
@@ -0,0 +1,29 @@
+var NotesApp = (function () {
+  var App = {
+    stores: {}
+  };
+
+  // Initialize localStorage
+  App.stores.notes = new Store('notes');
+
+  var Note = Backbone.Model.extend({
+    localStorage: App.stores.notes,
+
+    initialize: function () {
+      if (!this.get('title')) {
+        this.set({ title: "Note @ " + Date() });
+      }
+      if (!this.get('body')) {
+        this.set({ body: "No content" });
+      }
+    }
+  });
+
+  var Notes = Backbone.Collection.extend({
+
+  });
+
+  window.note = Note;
+
+  return App;
+})();
